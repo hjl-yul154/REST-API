8a1a05543295532527ec9ec374ab5166a73837ba
hjl-yul154 <hjl_liangyuxiao@126.com>
2021-10-25 23:48:05 -0700

    move db.init outside the __main__.

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1:5000/student/Rolf equals to @app.route('...')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+db.init_app(app)
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
