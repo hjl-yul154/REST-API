ece8083e8e531ba42ac4841fcbd7cfe837d9e360
hjl-yul154 <hjl_liangyuxiao@126.com>
2021-10-26 00:07:20 -0700

    created run.py.

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIOaNS'] = False # SQLALCHEMY has a better 
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
@@ -25,7 +22,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1:5000/student/Rolf equals to @app.route('...')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-db.init_app(app)
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
